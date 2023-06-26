761569d949869b0a1dbd55c9ddfb78bc0e343ee0
Roshan karki <rohitkc8848@gmail.com>
2023-06-27 04:55:41 +0545

    feat:feat:added the unhandled promise and the uncaughtexpection handler

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,84 @@
+import env from './utils/validateEnv'
 import express , {Request,Response}from 'express'
 import 'dotenv/config'
-import env from './utils/validateEnv'
 import connectDb from './config/connnectDb'
 import { errorHandler, notFound } from './middlewares/errorHandler'
+
+
+process.on("uncaughtException",(error:any)=>{
+    console.log(` Error:${error.message}`)
+    console.log("shuttting down the server due to uncaughtException ")
+    process.exit(1)
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// rest variables 
 const app=express()
 const PORT=env.PORT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // connecting to the database 
 connectDb()
 
+// middlewares
+app.use(express.json())
 
-app.get('/',(req:Request,res:Response)=>{
-    throw new Error("not found",)
-})
+// routes
+
+
+
+// error handlers
 app.use(errorHandler)
 app.use(notFound)
-app.listen(PORT,()=>{
-    console.log("running running ",PORT)
+const server =app.listen(PORT,()=>{
+    console.log(`Listening at the port ${PORT}`);
+}) 
+
+
+
+
+process.on("unhandledRejection",(error:any)=>{
+    console.log(` Error:${error.message}`)
+    console.log("shuttting down the server due to  unhandled promise rejection")
+    server.close(()=>{
+        process.exit(1);
+    })
 })
+
+
+
+
 
 
